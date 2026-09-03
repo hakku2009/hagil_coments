f1fa228dfc0cc85a4004ff943dc6b15e4f2568d9
hakku2009 <62943409+hakku2009@users.noreply.github.com>
2026-09-03 19:28:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -411,8 +411,10 @@ def teacher_comments():
     ])
 
 
+init_db()
+
+
 if __name__ == "__main__":
-    init_db()
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", "5000"))
     app.run(host=host, port=port, debug=False)
